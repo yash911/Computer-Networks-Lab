992b50721fcab7e0dd35c554006856161d82589c
yash911 <35463938+yash911@users.noreply.github.com>
2019-09-23 00:03:08 +0530

    Update Crc.java
    Final changes

--- Crc.java
+++ Crc.java
@@ -1,69 +1,71 @@
-import java.util.Scanner;
-
-public class Crc{
-    static int n, m, msb, i, j, k, g[], d[], z[], r[];
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the no of bits in data word");
-        n = s.nextInt();
-        System.out.println("Enter the no of bits in generating polynomial");
-        m = s.nextInt();
-
-        d = new int[m + n];
-        g = new int[n];
-
-        System.out.println("Enter data word");
-        for ( i = 0; i < n; i++) {
-            d[i] = s.nextInt();
-        }
-
-        System.out.println("Enter generating polynomial");
-        for ( i = 0; i < m; i++) {
-            g[i] = s.nextInt();
-        }
-
-        crcEval();
-
-        boolean flag = true;
-        for ( i = n; i < n+ m - 1; i++) {
-            if(r[i] == 1){
-                flag = false;
-            }
-        }
-        if(flag){
-            System.out.println("No Error");
-        }
-        else{
-            System.out.println("Error detected");
-        }
-
-    }
-
-    private static void crcEval(){
-        r = new int[m+n];
-        for (i = 0; i < m; i++) {
-            r[i] = d[i];
-        }
-        z = new int[m];
-        for (i = 0; i < n; i++) {
-            k = 0;
-            msb = r[i];
-            for (j = i; j < m; j++){
-                if(msb == 0){
-                    r[j] = r[j] ^ z[k];
-                }                
-                else{
-                    r[j] = r[j] ^ g[k];
-                }
-                k++;
-            }
-            r[m + i] = d[m + i];
-        }
-        System.out.println("Check sum is: " );
-        for (i = n; i < m+n-1; i++) {
-            d[i] = r[i];
-            System.out.print(d[i] + " ");
-        }
-        System.out.println();
-    }
+import java.util.*;
+class lab
+{
+	public static void main(String[] args)
+	{
+		String g,dw,tdw,ret,res;
+		int lg,ld,flag=1;
+		Scanner in =new Scanner(System.in);
+		System.out.println("enter the generator and the data word");
+		g=in.next();
+		dw=in.next();
+		ld=dw.length();
+		lg=g.length();
+		tdw=dw;
+		for(int i=0;i<lg-1;i++)
+			tdw=tdw.concat("0");
+		System.out.println("modified data word:"+tdw);
+		ret=crc(tdw,g,lg,ld);
+		System.out.println("check sum :"+ret);
+		tdw=dw+ret;
+		System.out.println("final code word:"+tdw);
+		System.out.println("sending........");
+		System.out.println("receiving......");
+		System.out.println("enter the recieved data");
+		res=in.next();
+		ret=crc(res,g,lg,ld);
+		System.out.println("check sum :"+ret);
+		for(int i=0;i<lg-1;i++)
+		if(ret.charAt(i)=='0')
+			flag=0;
+		else
+		{
+			flag=1;
+			break;
+		}
+		if(flag==0)
+			System.out.println("no error");
+		else
+			System.out.println("error");
+	}
+	public static String crc(String tdw,String g,int lg,int ld)
+	{
+		int e=lg;
+		String cs=tdw.substring(0,lg); 
+		while(e<=lg+ld-1)
+		{
+		if(cs.charAt(0)=='1')
+			cs=XOR(cs,g,lg);
+		cs=rem(cs,lg);
+		if(e<lg+ld-1)
+		cs+=tdw.charAt(e);
+		e++;
+		}
+		return cs;
+	}
+	
+	public static String XOR(String a,String b,int lg)
+	{
+		String v="";
+		for(int i=0;i<lg;i++)
+			v+=(a.charAt(i)==b.charAt(i))?'0':'1';
+		return v;	
+	}	
+	public static String rem(String a,int lg)
+	{
+		String v="";
+		for(int i=1;i<lg;i++)
+			v+=a.charAt(i);
+		return v;
+	}	
 }
